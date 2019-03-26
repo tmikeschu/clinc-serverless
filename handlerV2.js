17268ff04265ff7e35962fc95ff899f382376590
Mike Schutte <schutte@clinc.com>
2019-03-26 12:25:29 -0400

    Update change details sample

--- handlerV2.js
+++ handlerV2.js
@@ -1,10 +1,25 @@
 const { log } = require("./utils");
+const resolveChangeDetailsConfirmed = require("./resolveChangeDetailsConfirmed");
+
+const stateHandlers = {
+  change_details: resolveChangeDetailsConfirmed,
+  change_details_confirmed: resolveChangeDetailsConfirmed
+};
 
 module.exports = {
   run(event) {
     log("request", event.body);
 
-    // do stuff
+    const conversationData = JSON.parse(event.body);
+    const handler = stateHandlers[conversationData.state];
+    if (handler) {
+      const processed = handler(conversationData);
+      log("response", JSON.stringify(processed));
+      return Promise.resolve({
+        statusCode: 200,
+        body: JSON.stringify(processed)
+      });
+    }
 
     log("response", event.body);
     return Promise.resolve({
